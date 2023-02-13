789639344ec9292638211eacd2fa605b6870dd3a
Asif <asifsaheer7034@gmail.com>
2023-02-13 13:07:24 +0530

    updared

--- backend/server.js
+++ backend/server.js
@@ -60,4 +60,11 @@ db.connect((err) => {
   }
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, console.log(`server started on PORT ${PORT}`));
